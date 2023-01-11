6973d81ea33daf9559f27620fe2319bdf6cae82a
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-11 15:47:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AppMain from './components/AppMain.vue'
+
+export default{
+  
+}
+
+
+</script>
+
+<template>
+  <div>
+    <AppMain />
+  </div>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables' as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,23 @@
+<script>
+import AppMain from '../../../vite-comics/src/components/AppMain.vue';
+
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Main</h1>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
