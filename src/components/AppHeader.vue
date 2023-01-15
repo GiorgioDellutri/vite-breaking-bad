68ea74767162397dd0decbbf6d8bbf30eaca27cb
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-15 17:28:17 +0100

    moved axion call to AppHeader; import store

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,25 +1,57 @@
 <script>
+import axios from 'axios';
+import { store } from '../store';
 
 export default {
     name: 'AppHeader',
     data() {
         return {
-
+            store
         }
-    }
+    },
+    methods: {
+        getCardList() {
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0', {
+                params: {
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.data);
+                    this.store.cardList = response.data.data;
+                    console.log(this)
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    },
+
+    created() {
+        this.getCardList()
+    },
 }
 </script>
 
 <template>
-    <div>
-        <h1>Main</h1>
-        <p>
-            {{ store.searchText }}
-        </p>
-    </div>
+    <header>
+        <img src="../assets/img/Yu-Gi-Oh logo.webp" alt="">
+    </header>
+
 </template>
 
 
 <style lang="scss" scoped>
+header {
+    display: flex;
+    height: 10vh;
+    padding: 1rem;
+    font-size: 1.5rem;
 
+    img {
+        margin-left: .5rem;
+        height: 100%;
+        line-height: 10px;
+    }
+}
 </style>
