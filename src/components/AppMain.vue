2c763780a972694ec51c92a45862cae041c0f06f
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-15 18:28:40 +0100

    remove axios call & imported/added AppCard comp

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,46 +1,78 @@
 <script>
 import { store } from '../store';
-import axios from 'axios';
+import AppCard from '../components/AppCard.vue';
+
 
 
 export default {
     name: 'AppMain',
+
+    components: {
+        AppCard
+    },
     data() {
         return {
             store,
         }
     },
-    methods: {
-        getImage() {
-            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0', {
-                params: {
-                }
-            })
-                .then(function (response) {
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                })
-        }
-    },
-
-    created() {
-        this.getImage()
-    },
 }
 </script>
 
 <template>
-    <div>
-        <h1>Main</h1>
-        <p>
-            {{ store.searchText }}
-        </p>
-    </div>
+    <main>
+        <AppCard v-for="card in store.cardList" :singleCard="card" />
+    </main>
 </template>
 
 
 <style lang="scss" scoped>
-
+main {
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 1300px;
+    margin: 0 auto;
+}
 </style>
+
+
+
+
+// div.count-banner {
+//     padding: 1rem;
+//     background-color: #212529;
+
+//     p.card-count {
+//         font-size: 1.5rem;
+//         color: whitesmoke;
+//     }
+// }
+
+// div.card-container {
+//     display: flex;
+//     flex-wrap: wrap;
+//     border: 15px solid #212529;
+//     border-radius: 20px;
+//     max-width: 1300px;
+//     margin: 0 auto;
+//     margin-top: 2rem;
+//     padding: 1rem;
+//     text-align: center;
+//     font-weight: 500;
+
+//     div.card {
+//         width: calc(100% / 5 - 2rem);
+//         border: 4px solid #212529;
+//         border-radius: 10px;
+//         background-color: whitesmoke;
+//         padding: .8rem;
+//         margin: 1rem;
+
+//         img {
+//             width: 100%;
+//         }
+
+//         p.card-name {
+//             margin-top: 1rem;
+//         }
+//     }
+// }
