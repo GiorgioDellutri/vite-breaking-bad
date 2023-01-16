b1f9cb5c4978bb27bd4fac340be997df535b4fc2
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-16 16:57:17 +0100

    change AppCard name  in CardList

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,12 @@
 <script>
 import { store } from '../store';
-import AppCard from '../components/AppCard.vue';
-
-
+import CardList from './CardList.vue';
 
 export default {
     name: 'AppMain',
 
     components: {
-        AppCard
+        CardList
     },
     data() {
         return {
@@ -20,7 +18,7 @@ export default {
 
 <template>
     <main>
-        <AppCard v-for="card in store.cardList" :singleCard="card" />
+        <CardList />
     </main>
 </template>
 
@@ -33,46 +31,3 @@ main {
     margin: 0 auto;
 }
 </style>
-
-
-
-
-// div.count-banner {
-//     padding: 1rem;
-//     background-color: #212529;
-
-//     p.card-count {
-//         font-size: 1.5rem;
-//         color: whitesmoke;
-//     }
-// }
-
-// div.card-container {
-//     display: flex;
-//     flex-wrap: wrap;
-//     border: 15px solid #212529;
-//     border-radius: 20px;
-//     max-width: 1300px;
-//     margin: 0 auto;
-//     margin-top: 2rem;
-//     padding: 1rem;
-//     text-align: center;
-//     font-weight: 500;
-
-//     div.card {
-//         width: calc(100% / 5 - 2rem);
-//         border: 4px solid #212529;
-//         border-radius: 10px;
-//         background-color: whitesmoke;
-//         padding: .8rem;
-//         margin: 1rem;
-
-//         img {
-//             width: 100%;
-//         }
-
-//         p.card-name {
-//             margin-top: 1rem;
-//         }
-//     }
-// }
