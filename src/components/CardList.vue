b6e2956ab09dbefbac0d003af906b9c365eb4d89
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-16 16:57:41 +0100

    created CardList component

---
+++ src/components/CardList.vue
@@ -0,0 +1,24 @@
+<script>
+import { store } from '../store';
+import SingleCard from './SingleCard.vue';
+
+
+export default {
+    name: 'CardList',
+    props: {
+
+    },
+    components: {
+        SingleCard
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+<template>
+    <SingleCard v-for="singleCard in store.cardList" :cards="singleCard" />
+</template>
