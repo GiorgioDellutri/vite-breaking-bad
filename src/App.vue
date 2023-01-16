8a1c9a14d11412f5bc8273eb76b2023332cc1fb1
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-16 16:56:08 +0100

    moved call axios to AppVue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <script>
-import AppMain from './components/AppMain.vue'
-import AppHeader from './components/AppHeader.vue'
-
+import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+import { store } from './store';
 
 export default {
   components: {
     AppMain,
     AppHeader,
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    getCardList() {
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0', {
+        params: {
+        }
+      })
+        .then((response) => {
+          console.log(response.data.data);
+          this.store.cardList = response.data.data;
+          console.log(this)
+
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+  },
+
+  created() {
+    this.getCardList()
   },
 }
 </script>
@@ -23,8 +50,4 @@ export default {
 <style lang="scss">
 @use './styles/general.scss' as *;
 @use './styles/partials/variables' as *;
-
-main {
-  background-color: rebeccapurple;
-}
 </style>
